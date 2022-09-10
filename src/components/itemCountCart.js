126714c3f0ab998b4d4eda9e489923b7bf8441df
Franco Ciro Ferrari <francociroferrari@gmail.com>
2022-09-10 18:11:47 -0300

    Styles fixes

--- src/components/itemCountCart.js
+++ src/components/itemCountCart.js
@@ -29,7 +29,7 @@ export default function ItemCountCart({item}) {
   }
 
   return (
-<div className="w-3/5">
+<div className="w-2/4">
         <div
           className="flex w-fit text-gray-500 justify-start items-center px-0 py-0 border-indigo-600 border-0 rounded-md hover:bg-gray-50 ml-2">
           <button className="mr-0.5 px-2 pt-1 hover:bg-indigo-100" onClick={modifyDown}>-</button>
